903505e68cf76bce09234d05f8c33d5ccfc7e517
Fabian Montes <157678033+fabian-montes@users.noreply.github.com>
2025-08-17 14:22:11 -0600

    Update logginglogic.py

--- INFANTE/logger/logginglogic.py
+++ INFANTE/logger/logginglogic.py
@@ -2,12 +2,13 @@ import screenlogger
 import sendinginfo
 import time
 import json
+import subprocess
 from io import BytesIO
 from PIL import Image
 import os
 
-BASE_PATH = ""
-SEND_FILE_ENDPOINT = ""
+BASE_PATH = "http://localhost:3000"
+SEND_FILE_ENDPOINT = BASE_PATH + "/api/upload-image"
 
 # Ensure the photos folder exists
 os.makedirs("photos", exist_ok=True)
@@ -19,7 +20,7 @@ def save_image_and_metadata(payload, index):
     img = Image.open(BytesIO(img_bytes))
     
     # Save screenshot
-    img_filename = f"photos/screenshot_{index}.jpg"
+    img_filename = f"photos/screenshot_{0}.jpg"
     img.save(img_filename, format="JPEG")
     print(f"Saved screenshot to {img_filename}")
     
@@ -30,12 +31,27 @@ def save_image_and_metadata(payload, index):
         f.write(metadata_str)
     print(f"Saved metadata to {json_filename}")
 
+import subprocess
+
+def send_image_curl():
+    command = (
+        'curl.exe -X POST '
+        '-F "image=@C:\\Users\\fabia\\Documents\\Develop\\HackPuebla\\INFANTE\\logger\\photos\\screenshot_0.jpg" '
+        '-F "email=fab@gmail.com" '
+        'http://localhost:3000/api/upload-image'
+    )
+    subprocess.run(
+        ['powershell', '-Command', command],
+        text=True
+    )
+
 while True:
     print(counter)
     payload = screenlogger.log_current_window()
     print(payload)
-    save_image_and_metadata(payload, counter)
+    save_image_and_metadata(payload, 0)
     #sendinginfo.send_screenshot_jpeg(url=SEND_FILE_ENDPOINT, files=payload)
-
+    #time.sleep(1)
+    send_image_curl()
     counter += 1
-    time.sleep(3)  # 30,000 secondsprint(counter); adjust as needed
+    time.sleep(10)  # 30,000 secondsprint(counter); adjust as needed
