5376939f0ea11e3a4265ae460f4bf2cd2870c1a4
fabianmontes <fabianmontes11fab@gmail.com>
2025-08-17 09:40:07 -0600

    Mrjora a prompt de gemini

--- BACKEND/service/gemini.js
+++ BACKEND/service/gemini.js
@@ -1,6 +1,7 @@
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI, Type } from "@google/genai";
 import dotenv from 'dotenv';
 import * as fs from 'fs';
+import { type } from "os";
 
 dotenv.config();
 
@@ -18,11 +19,31 @@ function fileToGenerativePart(path, mimeType) {
 }
 
 async function runPrompt() {
-  const imagePart = fileToGenerativePart("C:/Users/fabia/Documents/Develop/HackPuebla/BACKEND/uploads/image-1755431860475-33355125.jpeg", "image/jpeg");
+  const imagePart = fileToGenerativePart("C:/Users/fabia/Documents/Develop/HackPuebla/BACKEND/uploads/image-1755444490349-567674403.jpeg", "image/jpeg");
 
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: [imagePart, "Explain this image"],
+    contents: [imagePart, "Which is the risk level of this content? Considering that should be seen by a child"],
+    config: {
+      systemInstruction: "You are a cibersecurity agent.",
+      responseMimeType: "application/json",
+      // temperature: 0.1,
+      responseSchema: {
+        type: Type.ARRAY,
+        items: {
+          type: Type.OBJECT,
+          properties: {
+            description: {
+              type: Type.STRING,
+            },
+            risk: {
+              type: Type.STRING,
+              enum: ["None", "Warning", "High"],
+            },
+          },
+        },
+      },
+    },
   });
 
   console.log(response.text);
